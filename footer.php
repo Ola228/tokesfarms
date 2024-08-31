22050018d377df957956f11c0fc9657ef361bbb8
Ola228 <148010847+Ola228@users.noreply.github.com>
2024-08-31 16:42:24 +0100

    first commit

---
+++ footer.php
@@ -0,0 +1,102 @@
+<footer class="bg-dark text-light" style="background-image: url(assets/img/shape/8.png);">
+        <div class="container">
+            <div class="f-items default-padding">
+                <div class="row">
+
+                    <!-- Single Itme -->
+                    <div class="col-lg-4 col-md-12 item">
+                        <div class="footer-item about">
+                            <img class="logo" src="assets/img/logo-light.png" alt="Logo">
+                            <p>
+                                At Toke's Agroenterprises, we are dedicated to uplifting women in aquaculture at every level. Through hands-on training and community engagement, we empower women to achieve success and build sustainable livelihoods.
+                            </p>
+                            <form action="#">
+                                <input type="email" placeholder="Your Email" class="form-control" name="email">
+                                <button type="submit"> Go</button>
+                            </form>
+                        </div>
+                    </div>
+                    <!-- End Single Itme -->
+
+                    <!-- Single Itme -->
+                    <div class="col-lg-2 col-md-6 item">
+                        <div class="footer-item link">
+                            <h4 class="widget-title">Explore</h4>
+                            <ul>
+                                <li>
+                                    <a href="about.php">About Us</a>
+                                </li>
+                                <li>
+                                    <a href="#">Meet Our Team</a>
+                                </li>
+                                <li>
+                                    <a href="#">Services</a>
+                                </li>
+                                <li>
+                                    <a href="contact.php">Contact Us</a>
+                                </li>
+                                <!-- <li>
+                                    <a href="team-details.html">Volunteers</a>
+                                </li> -->
+                            </ul>
+                        </div>
+                    </div>
+                    <!-- End Single Itme -->
+
+                    <!-- Single Itme -->
+
+                    <!-- End Single Itme -->
+
+                    <!-- Single Itme -->
+                    <div class="col-lg-6 col-md-6 item">
+                        <div class="footer-item contact">
+                            <h4 class="widget-title">Contact Info</h4>
+                            <ul>
+                                <li>
+                                    <div class="icon">
+                                        <i class="fas fa-home"></i>
+                                    </div>
+                                    <div class="content">
+                                        <strong>Address:</strong>
+                                        No. 26A, Ifokanbale community, Goodness Area <br> Lamodi Offa Kwara State Nigeria.
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="icon">
+                                        <i class="fas fa-envelope"></i>
+                                    </div>
+                                    <div class="content">
+                                        <strong>Email:</strong>
+                                        <a href="mailto:#">tokeagroenterise@gmail.com</a>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="icon">
+                                        <i class="fas fa-phone"></i>
+                                    </div>
+                                    <div class="content">
+                                        <strong>Phone:</strong>
+                                        <a href="tel:+2349025651416">+234 902 5651 416</a>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <!-- Single Itme -->
+
+                </div>
+            </div>
+            <!-- Start Footer Bottom -->
+            <div class="footer-bottom text-center">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <p>&copy; Copyright <?php echo date('Y') ?> All Rights Reserved by <a href="#">tokeagroenterise</a></p>
+                    </div>
+                </div>
+            </div>
+            <!-- End Footer Bottom -->
+        </div>
+        <div class="shape-right-bottom">
+            <img src="assets/img/shape/7.png" alt="Image Not Found">
+        </div>
+    </footer>
---
+++ nav.php
@@ -0,0 +1,150 @@
+    <!-- Start Preloader 
+    ============================================= -->
+    <div id="preloader">
+        <div id="agrica-preloader" class="agrica-preloader">
+            <div class="animation-preloader">
+                <div class="spinner"></div>
+            </div>
+            <div class="loader">
+                <div class="row">
+                    <div class="col-3 loader-section section-left">
+                        <div class="bg"></div>
+                    </div>
+                    <div class="col-3 loader-section section-left">
+                        <div class="bg"></div>
+                    </div>
+                    <div class="col-3 loader-section section-right">
+                        <div class="bg"></div>
+                    </div>
+                    <div class="col-3 loader-section section-right">
+                        <div class="bg"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End Preloader -->
+
+    <!-- Start Header Top 
+    ============================================= -->
+    <div class="top-bar-area top-bar-style-one bg-dark text-light">
+        <div class="container">
+            <div class="row align-center">
+                <div class="col-lg-8">
+                    <ul class="item-flex">
+                        <li>
+                            <i class="fas fa-clock"></i> Opening Hours : Sunday- Friday, 08:00 am - 05:00pm
+                        </li>
+                        <li>
+                            <a href="tel:+2349025651416"><i class="fas fa-phone-alt"></i> +234 902 565 1416</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-lg-4 text-end">
+                    <div class="social">
+                        <ul>
+                            <li>
+                                <a href="#">
+                                    <i class="fab fa-facebook-f"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <i class="fab fa-twitter"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <i class="fab fa-whatsapp"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <i class="fab fa-linkedin-in"></i>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End Header Top -->
+
+    <!-- Header 
+    ============================================= -->
+    <header>
+        <!-- Start Navigation -->
+        <nav class="navbar mobile-sidenav inc-shape navbar-sticky navbar-default validnavs dark">
+
+            <div class="container d-flex justify-content-between align-items-center">
+                
+                
+                <div class="navbar-brand-left">
+                    <!-- Start Header Navigation -->
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-menu">
+                            <i class="fa fa-bars"></i>
+                        </button>
+                        <a class="navbar-brand" href="index-2.html">
+                            <img src="assets/img/logo-mix.png" class="logo" alt="Logo">
+                        </a>
+                    </div>
+                    <!-- End Header Navigation -->
+                </div>
+
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="navbar-menu">
+
+                    <img src="assets/img/logo.png" alt="Logo">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-menu">
+                        <i class="fa fa-times"></i>
+                    </button>
+                    
+                    <ul class="nav navbar-nav navbar-right" data-in="fadeInDown" data-out="fadeOutUp">
+                        <!-- <li class="dropdown">
+                            <a href="#" class="dropdown-toggle active" data-toggle="dropdown" >Home</a>
+                            <ul class="dropdown-menu">
+                                <li><a href="index-2.html">Home Fishery</a></li>
+                                <li><a href="index-3.html">Home Poultry</a></li>
+                                <li><a href="index-4.html">Home Vegetable</a></li>
+                            </ul>
+                        </li> -->
+                        <li class="dropdown">
+                            <a href="index.php" >Home</a>
+                            
+                        </li>
+                        <li class="dropdown">
+                            <a href="about.php" >About Us</a>
+                            
+                        </li>
+                        <li class="dropdown">
+                            <a href="project.php" >Our Projects</a>
+                            
+                        </li>
+                        <li class="dropdown">
+                            <a href="service.php" >Our Services</a>
+                            
+                        </li>
+                       
+                       
+                        <li class="dropdown">
+                            <a href="#" >Our Blog</a>
+                            
+                        </li>
+                       
+                    </ul>
+                </div><!-- /.navbar-collapse -->
+
+               
+
+                <!-- Main Nav -->
+            </div>   
+            <!-- Overlay screen for menu -->
+            <div class="overlay-screen"></div>
+            <!-- End Overlay screen for menu -->
+
+        </nav>
+        <!-- End Navigation -->
+    </header>
+    <!-- End Header -->
